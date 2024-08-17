961d8e478163fc56ab1cda411a6f794e95a3ed79
Angad88 <121978333+Angad88@users.noreply.github.com>
2024-08-16 17:40:36 -0700

    FinalFinished

---
+++ Final/Question1.js
@@ -0,0 +1,25 @@
+// Question 1 Code
+
+function Question1Reversing(k) {
+    let NegativeCheck;
+    if (k < 0) {
+        NegativeCheck = -1;
+    }   
+    else {
+        NegativeCheck = 1;
+    }
+    let NumberReversed;
+    
+    NumberReversed = parseInt(Math.abs(k).toString().split('').reverse().join('')) * NegativeCheck;
+    return NumberReversed;
+}
+
+let testNumber1;
+testNumber1 = 123;
+console.log(`The First number: ${testNumber1}`);
+console.log(`The First number Reversed: = ${Question1Reversing(testNumber1)}`);
+
+let testNumber2;
+testNumber2 = -456;
+console.log(`The First number: ${testNumber2}`);
+console.log(`The First number Reversed: = ${Question1Reversing(testNumber2)}`);
---
+++ Final/Question4.js
@@ -0,0 +1,35 @@
+// Question 4 Code
+
+function Question4ArrayDuplicate(theArray) {
+    let lowestNum
+    lowestNum = Infinity;
+
+    let howOften
+    howOften = {};
+
+    
+    for (let n of theArray) {
+        if (howOften[n]) {
+            lowestNum = Math.min(lowestNum, n);
+        } else {
+            howOften[n] = 1;
+        }
+    }
+    return lowestNum;
+}
+
+let testArray1;
+let smallestDuplicate1;
+testArray1 = [4, 10, 5, 1, 11, 5, 1, 4, 1]
+smallestDuplicate1 = Question4ArrayDuplicate(testArray1);
+
+let testArray2;
+let smallestDuplicate2;
+testArray2 = [1, 10, 1, -1, 10, -1]
+smallestDuplicate2 = Question4ArrayDuplicate(testArray2);
+
+console.log(`First Array: [${testArray1}]`)
+console.log(`Smallest Duplicate found in First Array: ${smallestDuplicate1}`)
+
+console.log(`First Array: [${testArray2}]`)
+console.log(`Smallest Duplicate found in First Array: ${smallestDuplicate2}`)
